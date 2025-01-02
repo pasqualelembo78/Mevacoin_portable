7038303766b716c831216f876a8811769c8282bd
pasqualelembo78 <55604527+pasqualelembo78@users.noreply.github.com>
2025-01-02 20:46:28 +0100

    Update MevaCoinConfig.h
    Polvere e fee

--- src/MevaCoinConfig.h
+++ src/MevaCoinConfig.h
@@ -25,7 +25,7 @@ const size_t   BLOCKCHAIN_TIMESTAMP_CHECK_WINDOW             = 60;
 //TODO ((uint64_t)(-1)) equals to 18446744073709551616 coins
 //TODO or you can define number explicitly UINT64_C(858986905600000000)
 const uint64_t MONEY_SUPPLY                                  = (uint64_t)(-1);
-const unsigned EMISSION_SPEED_FACTOR                         = 35;
+const unsigned EMISSION_SPEED_FACTOR                         = 33;
 static_assert(EMISSION_SPEED_FACTOR <= 8 * sizeof(uint64_t), "Bad EMISSION_SPEED_FACTOR");
 
 //TODO Define number of blocks for block size median calculation
@@ -35,8 +35,8 @@ const size_t   MEVACOIN_COINBASE_BLOB_RESERVED_SIZE        = 600;
 //TODO Define number of digits
 const size_t   MEVACOIN_DISPLAY_DECIMAL_POINT              = 8;
 //TODO Define minimum fee for transactions
-const uint64_t MINIMUM_FEE                                   = 100000;
-const uint64_t DEFAULT_DUST_THRESHOLD                        = MINIMUM_FEE;
+const uint64_t MINIMUM_FEE                                   = 10000;
+const uint64_t DEFAULT_DUST_THRESHOLD                        = 100;
 
 //TODO Define preferred block's target time
 const uint64_t DIFFICULTY_TARGET                             = 120; // seconds
