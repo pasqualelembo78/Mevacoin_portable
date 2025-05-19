82abd83fd6a5f6935a3f1ea8dd29d5c5ea7992fa
pasqualelembo78 <55604527+pasqualelembo78@users.noreply.github.com>
2025-05-19 07:12:00 +0200

    Update Install_noboost.sh

--- Install_noboost.sh
+++ Install_noboost.sh
@@ -15,9 +15,9 @@ echo "Configurazione del firewall UFW..."
 sudo ufw allow 22/tcp         # SSH
 sudo ufw allow 21/tcp         # FTP
 sudo ufw allow 4000/tcp       # NoMachine
-sudo ufw allow 18080/tcp      # Mevacoin P2P
-sudo ufw allow 18081/tcp      # Mevacoin RPC
-sudo ufw allow 18082/tcp      # Mevacoin RPC secondario
+sudo ufw allow 17080/tcp      # Mevacoin P2P
+sudo ufw allow 17081/tcp      # Mevacoin RPC
+sudo ufw allow 17082/tcp      # Mevacoin RPC secondario
 sudo ufw allow 'Apache'       # HTTP (porta 80)
 sudo ufw allow 'Apache Full'  # HTTP + HTTPS (porte 80 e 443)
 sudo ufw --force enable
